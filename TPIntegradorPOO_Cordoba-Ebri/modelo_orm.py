f22261558338a7ce3379d14aba65d7a6582da2a3
IvanaEbri <3ivana.ebri@gmail.com>
2023-06-14 20:30:47 -0300

    correccion nombre carpeta para conexion de db

---
+++ TPIntegradorPOO_Cordoba-Ebri/modelo_orm.py
@@ -0,0 +1,11 @@
+pip install peewee
+from peewee import *
+
+sqlite_db = SqliteDatabase('./TPIntegradorPOO_Cordoba-Ebri/Obras_CABA.db', pragmas={'journal_mode': 'wal'})
+try:
+    """Intento de conexion a la base de datos"""
+    sqlite_db.connect()
+except OperationalError as e:
+    print("Error al conectar con la BD.", e)
+    exit()
+
